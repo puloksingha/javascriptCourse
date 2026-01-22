6536e49271ef0a461e100b4f39c8ab721a5c03f0
pulok singha <puloksingha2018@gmail.com>
2026-01-22 17:02:36 +0530

    first commit

---
+++ day1/README.md
@@ -0,0 +1,43 @@
+# Day 1 - JavaScript Course
+
+## Topics Covered
+
+- Introduction to JavaScript
+- Using `console.log()` to print output
+- Variables in JavaScript (`var`, `let`, `const`)
+- Comments (single-line and multi-line)
+
+## Notes
+
+### Console Output
+- `console.log()` is used to print messages to the console or terminal
+- Useful for debugging and displaying information
+
+### Variables
+- **Variable**: A container for storing data values
+- **`let`**: Declares a variable that can be reassigned
+- **`const`**: Declares a constant variable that cannot be reassigned
+- **`var`**: Older way to declare variables (function-scoped)
+
+### Comments
+- Single-line comments: `// comment`
+- Multi-line comments: `/* comment */`
+
+## Code Examples
+
+See [script.js](script.js) for practical examples including:
+- Basic console.log usage
+- Variable declarations with let, const, and var
+- Variable reassignment
+- Comment syntax
+
+## Resources
+
+- [MDN JavaScript Guide](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide)
+- [JavaScript.info](https://javascript.info/)
+
+## Exercises
+
+- Practice declaring variables with different keywords
+- Experiment with changing variable values
+- Try console.log with different data types
---
+++ day1/script.js
@@ -0,0 +1,30 @@
+console.log("Hello, Learner") // console.log is use to print something in console or terminal
+console.log("Welcome to JavaScript programming!") // Another example of printing a message
+
+// You can run this script using Node.js or in the browser console
+// To run in Node.js, save this file as script.js and execute `node script.js` in your terminal 
+// To run in the browser, open the developer console and paste the code there.
+
+//what is variable in javascript
+//A variable is a container for storing data values. In JavaScript, you can declare a variable using var, let, or const keywords.   
+let message = "This is a variable in JavaScript"
+console.log(message) // Output the value of the variable
+const pi = 3.14 // constant variable
+console.log("Value of pi:", pi)
+// You can change the value of a variable declared with let
+message = "Variable value changed!"
+console.log(message)
+// But you cannot change the value of a constant variable declared with const
+// pi = 3.14159 // This will throw an error     
+var count = 10 // variable declared with var
+console.log("Count:", count)
+count = 20 // changing the value of var variable
+console.log("Updated Count:", count)    
+// This is a simple demonstration of variables in JavaScript.
+
+// this is a single line comment
+/*
+This is a multi-line comment
+You can write comments spanning multiple lines
+*/
+console.log("End of the script")
